8cfc401ff9daf1d62acef7931550e2675c934e6c
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-09-15 13:01:15 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -1,1 +1,11 @@
+// Task 1: Initialize inventory with product objects
+
+let inventory = [
+    {name: "goPro", price: 89.99, quantity: 50, lowStockLevel: 8},
+    {name: "Lenovo Laptop", price: 799.99, quantity: 40, lowStockLevel: 5}, 
+    {name: "Speaker Bar", price: 199.99, quantity: 25, lowStockLevel: 4}, 
+    {name: "Apple Watch" price: 399.99, quantity: 45, lowStockLevel: 8}, 
+    {name: "Sony Television", price: 649.99, quantity: 11, lowStockLevel: 6}];
+
+    console.log(inventory);
 
